35c0baa3fe350b46b066a63c5a14fa7d5e4a1de2
Philliesdog <Philliesdog123@gmail.com>
2025-10-26 03:42:59 -0400

    this implements mine adjacency calculations

--- minesweeper.c
+++ minesweeper.c
@@ -55,12 +55,43 @@ void init_game() {
     }
 }
 
+void place_mines(int avoid_x, int avoid_y) {
+    int mines_placed = 0;
+    
+    while (mines_placed < MINE_COUNT) {
+        int x = rand() % GRID_WIDTH;
+        int y = rand() % GRID_HEIGHT;
+        
+        if (!game.grid[y][x].is_mine && !(x == avoid_x && y == avoid_y)) {
+            game.grid[y][x].is_mine = true;
+            mines_placed++;
+        }
+    }
+    
+    for (int y = 0; y < GRID_HEIGHT; y++) {
+        for (int x = 0; x < GRID_WIDTH; x++) {
+            if (game.grid[y][x].is_mine) continue;
+            
+            int count = 0;
+            for (int dy = -1; dy <= 1; dy++) {
+                for (int dx = -1; dx <= 1; dx++) {
+                    int nx = x + dx;
+                    int ny = y + dy;
+                    if (nx >= 0 && nx < GRID_WIDTH && ny >= 0 && ny < GRID_HEIGHT) {
+                        if (game.grid[ny][nx].is_mine) count++;
+                    }
+                }
+            }
+            game.grid[y][x].adjacent_mines = count;
+        }
+    }
+}
+
 int main(int argc, char* argv[]) {
     srand((unsigned int)time(NULL));
     
     window = SDL_CreateWindow("Minesweeper", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
                               WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
-    
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
     
     init_game();
@@ -83,7 +114,6 @@ int main(int argc, char* argv[]) {
     
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
-    TTF_Quit();
     SDL_Quit();
     
     return 0;
